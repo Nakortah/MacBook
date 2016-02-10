52fc402812c32a3e5ff6311a7ec5d61067b812c7
Nakortah <aleksey.tyurin@corp.badoo.com>
2016-02-10 11:21:14 +0300

    Delete MacBookInventory.sh

--- MacBookInventory.sh
+++
@@ -1,1 +0,0 @@
-(system_profiler SPHardwareDataType | grep -E "Model|Processor |Memory|Serial|UUID" &&  diskutil info disk0 | grep -E "Media Name| Total" && echo "****************************************") >> MacBookInventory.txt
