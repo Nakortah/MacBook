5bb5ea4697946c1ca53e5de08045c0aba218cb83
Nakortah <aleksey.tyurin@corp.badoo.com>
2016-02-10 11:29:41 +0300

    Create MacBookInventory.sh

---
+++ MacBookInventory.sh
@@ -0,0 +1,1 @@
+(system_profiler SPHardwareDataType | grep -E "Model|Processor |Memory|Serial|UUID" &&  diskutil info disk0 | grep -E "Media Name| Total" && echo "****************************************")  >> MacBookInventory.txt
