edca6d74707b7a2b876f35cd9a7a8e5ca287c252
A.Tyurin <aleksey.tyurin@corp.badoo.com>
2016-02-10 12:08:14 +0300

    Add #!/ban/bash

--- MacBookInventory.sh
+++ MacBookInventory.sh
@@ -1,1 +1,2 @@
+#!bin/bash
 (system_profiler SPHardwareDataType | grep -E "Model|Processor |Memory|Serial|UUID" &&  diskutil info disk0 | grep -E "Media Name| Total" && echo "****************************************")  >> MacBookInventory.txt
